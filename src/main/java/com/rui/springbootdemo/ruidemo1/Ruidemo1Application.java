9733c44950af0f63e175ec4616bfa22cce134bb9
zhangrui <616819363@qq.com>
2021-06-03 15:25:08 +0800

    测试消息

---
+++ src/main/java/com/rui/springbootdemo/ruidemo1/Ruidemo1Application.java
@@ -0,0 +1,15 @@
+package com.rui.springbootdemo.ruidemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruidemo1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Ruidemo1Application.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/rui/springbootdemo/ruidemo1/Ruidemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rui.springbootdemo.ruidemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ruidemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
